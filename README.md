1cc8a55a0b5b559f0c645fa10cc8aa3ad3af2220
Christian Merkwirth <Christian.Merkwirth@nauto.com>
2017-12-20 02:30:47 +0100

    .

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rtools
+http://www.mjdenny.com/Rcpp_Intro.html
+https://teuder.github.io/rcpp4everyone_en/
---
+++ setup.R
@@ -0,0 +1,12 @@
+# Install most important packages for R.
+
+# The one tool to get it all started.
+install.packages("devtools")
+# Tidyverse to get Hadley's finest tools.
+install.packages("tidyverse")
+# Super fast data.table.
+install.packages("data.table")
+# Fast serialization/deserialization.
+install.packages(c("feather", "fst"))
+install.packages("plotly")
+
