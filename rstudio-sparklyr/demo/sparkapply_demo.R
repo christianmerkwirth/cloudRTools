c98e2eb6391da977b4175dacfd9e0f8755e10e91
Christian Merkwirth <Christian.Merkwirth@nauto.com>
2018-02-05 00:27:56 +0100

    Modified sparkapply_demo

--- rstudio-sparklyr/demo/sparkapply_demo.R
+++ rstudio-sparklyr/demo/sparkapply_demo.R
@@ -4,21 +4,36 @@ library(lubridate)
 library(ggplot2)
 library(sparklyr)
 
+# Setting environment vars for Spark.
+Sys.setenv(SPARK_HOME="/usr/lib/spark")
+Sys.setenv(JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/jre/")
+
 config <- spark_config()
-config[["spark.r.command"]] <- "/usr/bin/R"
 
-sc <- spark_connect(master = "local")
+# Connect to spark on master node
+sc <- spark_connect(master = "yarn-client", spark_home = "/usr/lib/spark")
 
-trees_tbl <- sdf_copy_to(sc, trees, repartition = 2)
+
+trees_tbl <- sdf_copy_to(sc, trees, repartition = 2, overwrite = TRUE)
 
 trees_tbl %>%
   spark_apply(function(e) scale(e))
 
-iris_tbl <- copy_to(sc, iris)
+iris_tbl <- copy_to(sc, iris, repartition = 2, overwrite = TRUE)
+
+
+iris_func <- function(e) {
+  if (!("broom" %in% installed.packages())) {
+    install.packages("broom")
+  }
+  library(broom)
+  broom::tidy(lm(Petal_Width ~ Petal_Length, e))
+}
 
 spark_apply(
   iris_tbl,
-  function(e) { broom::tidy(lm(Petal_Width ~ Petal_Length, e)) },
+  iris_func,
   names = c("term", "estimate", "std.error", "statistic", "p.value"),
-  group_by = "Species"
+  group_by = "Species",
+  packages = FALSE
 )
